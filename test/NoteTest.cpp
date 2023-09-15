9230a1882abdb875c5c6d66bd28ab41bfd563e77
marco-3636 <marco.siani@stud.unifi.it>
2023-09-15 11:25:13 +0200

    modifica fatta, manca eseguibile

--- test/NoteTest.cpp
+++ test/NoteTest.cpp
@@ -1,3 +1,24 @@
 //
 // Created by m36 on 15/09/23.
 //
+#include "gtest/gtest.h"
+
+#include "../Note.h"
+
+
+TEST(Notes, DefaultConstructor) {
+Notes TestNote  = Notes("Titolo nota 1", "Testo nota 1", false);
+ASSERT_FALSE(" "==TestNote.getText());
+ASSERT_FALSE(" "==TestNote.getTitle());
+ASSERT_TRUE(TestNote.isLocked()==true || TestNote.isLocked()==false);
+}
+
+TEST(Notes, GetterAndSetterTester) {
+Notes TestNote  = Notes("Titolo nota 1", "Testo nota 1", false);
+TestNote.setTitle("Titolo di Prova");
+TestNote.setText("Testo di Prova");
+TestNote.setLocked(true);
+ASSERT_EQ("Titolo di Prova", TestNote.getTitle());
+ASSERT_EQ("Testo di Prova", TestNote.getText());
+ASSERT_TRUE(TestNote.isLocked());
+}
--- test/NotebookTest.cpp
+++ test/NotebookTest.cpp
@@ -1,3 +1,32 @@
 //
 // Created by m36 on 15/09/23.
 //
+#include "gtest/gtest.h"
+#include "../Notebook.h"
+#include "../SetCollezioni.h"
+
+
+TEST(Collezione, ConstructorsTest) {
+Collezione colTest1("Titolo di prova");
+std::string TitoloProva = "Titolo Prova";
+Collezione colTest2(TitoloProva);
+//test per primo costruttore
+EXPECT_EQ("Titolo di prova", colTest1.getTitolo());
+EXPECT_EQ(0, colTest1.getTotalNotes());
+EXPECT_EQ(0, colTest1.getTotalLockedNotes());
+//test per secondo costruttore
+EXPECT_EQ("Titolo Prova", colTest2.getTitolo());
+EXPECT_EQ(0, colTest2.getTotalNotes());
+EXPECT_EQ(0, colTest2.getTotalLockedNotes());
+}
+
+TEST(Collezione, ClassFunctionsTest){
+Notes notaTest = Notes("Titolo nota 1", "Testo nota 1", false);
+auto* ColTest = new Collezione("Titolo di prova");
+auto* e = new Executive(ColTest);
+e->AddNote(notaTest);
+EXPECT_EQ(1, ColTest->CollectionSize());
+ASSERT_TRUE(nullptr != ColTest->getNote(0));
+EXPECT_EQ(1, ColTest->getTotalNotes());
+EXPECT_EQ(0, ColTest->getTotalLockedNotes());
+}
